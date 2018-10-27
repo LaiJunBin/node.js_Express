a5c4a7ad931d1acc3ec0a88156efa5d53f859b8c
xyz607xx <xyz607xx@gmail.com>
2018-10-27 19:03:53 +0800

    api router

---
+++ blog/routes/api.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+var mysql = require('../database');
+var crypto = require('crypto');
+
+
+router.post('/login', function (req, res, next) {
+    
+});
+
+module.exports = router;
